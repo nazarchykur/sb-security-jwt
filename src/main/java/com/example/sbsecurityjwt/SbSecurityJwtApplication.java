c2a8f11627172ebf7d88b84503f7f02c976eaf9b
nazarchykur <nazarchykur@gmail.com>
2023-06-21 09:38:45 +0300

    init sb-security-jwt

---
+++ src/main/java/com/example/sbsecurityjwt/SbSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSecurityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbSecurityJwtApplication.class, args);
+    }
+
+}
